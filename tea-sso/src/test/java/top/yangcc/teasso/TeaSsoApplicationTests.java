8b204fd7b08fd493a8a37aef18948b02eb8745bf
chalmery <95135709+chalmery@users.noreply.github.com>
2024-03-18 21:25:19 +0800

    add:sso module

---
+++ tea-sso/src/test/java/top/yangcc/teasso/TeaSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package top.yangcc.teasso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
